c424a748cfb99b439436c9dbbf2ed23d77fa62b3
RebelDroid09 <paulcs91@gmail.com>
2020-11-24 21:16:02 -0600

    Update app.py
    wrote initial code for pulling the scraped data into the mongo database.

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,14 +1,23 @@
 # 1. import Flask
 from flask import Flask
 from scrape_mars.py import scrape
+import pymongo
 
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
+
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
 
 
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def home():
+
+    db = client.marsDatabase
+    collection = db.items
+
+    
     print("Server received request for 'Home' page...")
     return "Welcome to the temporary home page!"
 
@@ -16,7 +25,20 @@ def home():
 # 4. Define what to do when a user hits the /about route
 @app.route("/scrape")
 def scrape():
-    marsDetails = scrape()
+    marsDetails = scrape()    
+
+    db = client.marsDatabase
+    collection = db.items
+
+    marsData = {
+        'news': marsDetails[0],
+        'featuredImage': marsDetails[1],
+        'facts': marsDetails[2],
+        'hemispheres': marsDetails[3]
+    }
+
+    collection.insert_one(marsData)
+
     return "Scraping data..."
 
 
